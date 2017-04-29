c68c999d7004184d594497de21c38ddec678e2ba
Manalalmaeen <alma9199@stthomas.edu>
2017-04-29 13:38:49 -0500

    JUnit

---
+++ src/Testing.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+	public class AllTests {
+		@Test
+		public void setUp()throws Exception
+		{
+			
+		}
+		@Test
+		public void testManalAddSeeds()
+		{
+			 Storehouse  house = new  Storehouse ();
+			 assertEquals("test initial seeds",0,house.getNumSeeds());
+			 
+			 house.addCapturedSeeds(5);
+			 assertEquals("Capture 5 seeds", 5, house.getNumSeeds());
+
+		}
+			
+		}
+}
