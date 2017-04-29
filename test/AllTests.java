6ce55980e7cdfaa20b60a32632bb13815c6823f9
koha3496 <koha3496@OSS326-01.stthomas.edu>
2017-04-29 13:56:10 -0500

    Deleted AllTests and PlayerTest (without initials)

--- test/AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ BoardTest.class, MoveTest.class, PitTest.class, PlayerTest.class })
-public class AllTests {
-
-}
